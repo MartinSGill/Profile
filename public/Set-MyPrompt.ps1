921bf168dcbeb016ccbf7b3ececea974e3a89e48
Martin Gill <martin@martinsgill.co.uk>
2020-03-01 18:54:23 +0100

    Refactored prompt, fixed char issue for terminal

--- public/Set-MyPrompt.ps1
+++ public/Set-MyPrompt.ps1
@@ -1,4 +1,78 @@
-﻿function Set-MyPrompt {
+﻿
+
+$promptElements = @{
+    DirStack = {
+        if($pushed = (Get-Location -Stack).count) {
+            $pArgs = @{
+                InputObject = "&raquo;$pushed"
+                ForegroundColor = "White"
+                BackgroundColor = "SlateBlue"
+            }
+            New-PromptText @pArgs
+        }
+    }
+
+    Path = { New-PromptText (Get-PromptPathString) -ForegroundColor "Black" -BackgroundColor "White" }
+
+    LastCommandTime = {
+        if (-not [string]::IsNullOrWhiteSpace($(Get-Elapsed)) ) {
+            $useSafeChars = [bool]$env:PROFILE_SAFE_CHARS
+            $pArgs = @{
+                InputObject = " 祥 $(Get-Elapsed -Format "{0:ss\.ffff}") "
+                ForegroundColor = "Black"
+                BackgroundColor = "#E7FFAC"
+                ErrorForegroundColor = "Black"
+                ErrorBackgroundColor = "#FFABAB"
+            }
+
+            if ($useSafeChars) {
+                $pArgs.InputObject = " $(Get-Elapsed -Format "{0:ss\.ffff}") "
+            }
+
+            New-PromptText @pArgs
+        }
+    }
+
+    AlignRight = { "`t" }
+
+    NewLine =  { "`n" }
+
+    VcsStatus = { New-PromptText -InputObject "$(Write-VcsStatus)" -ForegroundColor "white" -BackgroundColor "black" }
+
+    Time = {
+        $useSafeChars = [bool]$env:PROFILE_SAFE_CHARS
+        $pArgs = @{
+            InputObject = "  $(Get-Date -Format "T") "
+            # InputObject = " T $(Get-Date -Format "T") "
+            ForegroundColor = "Black"
+            BackgroundColor = "#BFFCC6"
+        }
+        if ($useSafeChars) {
+            $pArgs.InputObject = " $(Get-Date -Format "T") "
+        }
+        New-PromptText @pArgs
+    }
+
+    Prompt = {
+        $useSafeChars = [bool]$env:PROFILE_SAFE_CHARS
+        $pArgs = @{
+            InputObject = " $($PSVersionTable.PSVersion.Major)  "
+            #InputObject = " $($PSVersionTable.PSVersion.Major) ﲵ"
+            ForegroundColor = "Black"
+            BackgroundColor = "#6EB5FF"
+            ElevatedForegroundColor = "Black"
+            ElevatedBackgroundColor = "#FFABAB"
+        }
+        if ($useSafeChars) {
+            $pArgs.InputObject = " $($PSVersionTable.PSVersion.Major) >_"
+        }
+        New-PromptText @pArgs
+    }
+}
+
+
+
+function Set-MyPrompt {
     <#
     .SYNOPSIS
         Configures preferred prompt format.
@@ -11,14 +85,8 @@
     # Clear out any existing settings
     Remove-Item -Path (Get-ConfigurationPath -Module (Get-Module Powerline)) -Force -Recurse
 
-    if ([string]::IsNullOrWhiteSpace($env:PROFILE_SAFE_CHARS))
-    {
-        $useSafeChars = $false
-    } else {
-        $useSafeChars = [bool]$env:PROFILE_SAFE_CHARS
-    }
-
     if (Get-Module posh-git) {
+        $useSafeChars = [bool]$env:PROFILE_SAFE_CHARS
         if ($useSafeChars) {
             $GitPromptSettings.BeforeStatus.Text = "["
             $GitPromptSettings.DelimStatus.Text = " |"
@@ -39,70 +107,14 @@
         Save = $false
     }
 
-    $plArgs.Prompt += {
-        if($pushed = (Get-Location -Stack).count) {
-            $pArgs = @{
-                InputObject = "&raquo;$pushed"
-                ForegroundColor = "White"
-                BackgroundColor = "SlateBlue"
-            }
-            New-PromptText @pArgs
-        }
-    }
-
-    $plArgs.Prompt += { New-PromptText (Get-PromptPathString) -ForegroundColor "Black" -BackgroundColor "White" }
-
-    # $plArgs.Prompt += { "`t" }
-
-    $plArgs.Prompt += {
-        if (-not [string]::IsNullOrWhiteSpace($(Get-Elapsed)) ) {
-            $pArgs = @{
-                InputObject = " 祥 $(Get-Elapsed -Format "{0:ss\.ffff}") "
-                ForegroundColor = "Black"
-                BackgroundColor = "#E7FFAC"
-                ErrorForegroundColor = "Black"
-                ErrorBackgroundColor = "#FFABAB"
-            }
-
-            if ($useSafeChars) {
-                $pArgs.InputObject = " $(Get-Elapsed -Format "{0:ss\.ffff}") "
-            }
-
-            New-PromptText @pArgs
-        }
-    }
-
-    if (Get-Module posh-git) {
-        $plArgs.Prompt += { New-PromptText -InputObject "$(Write-VcsStatus)" -ForegroundColor "white" -BackgroundColor "black" }
-    }
-
-    $plArgs.Prompt += { "`n" }
-
-    $plArgs.Prompt += {
-        $pArgs = @{
-            InputObject = "  $(Get-Date -Format "T") "
-            ForegroundColor = "Black"
-            BackgroundColor = "#BFFCC6"
-        }
-        if ($useSafeChars) {
-            $pArgs.InputObject = " $(Get-Date -Format "T") "
-        }
-        New-PromptText @pArgs
-    }
-
-    $plArgs.Prompt += {
-        $pArgs = @{
-            InputObject = " $($PSVersionTable.PSVersion.Major) ﲵ"
-            ForegroundColor = "Black"
-            BackgroundColor = "#6EB5FF"
-            ElevatedForegroundColor = "Black"
-            ElevatedBackgroundColor = "#FFABAB"
-        }
-        if ($useSafeChars) {
-            $pArgs.InputObject = " $($PSVersionTable.PSVersion.Major) >_"
-        }
-        New-PromptText @pArgs
-    }
+    $plArgs.Prompt += $promptElements.DirStack
+    $plArgs.Prompt += $promptElements.Path
+    # $plArgs.Prompt += $promptElements.AlignRight
+    $plArgs.Prompt += $promptElements.LastCommandTime
+    if (Get-Module posh-git) { $plArgs.Prompt += $promptElements.VcsStatus }
+    $plArgs.Prompt += $promptElements.NewLine
+    $plArgs.Prompt += $promptElements.Time
+    $plArgs.Prompt += $promptElements.Prompt
 
     Set-PowerLinePrompt @plArgs
     Trace-Message "Prompt Set."
