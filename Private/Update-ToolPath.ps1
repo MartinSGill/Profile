577e97cb3be2db303846b7da425718ed7f2ead18
Martin Gill <martin@martinsgill.co.uk>
2022-01-16 14:01:46 +0100

    Better alias/tool handling, with dbox support

--- Private/Update-ToolPath.ps1
+++ Private/Update-ToolPath.ps1
@@ -1,41 +1,50 @@
 function script:Update-ToolPath {
     <#
         .Synopsis
-            Add useful things to the PATH which aren't normally there on Windows.
-        .Description
-            Add Tools, Utilities, or Scripts folders which are in your profile to your Env:PATH variable
-            Also adds the location of msbuild, merge, tf and python, as well as iisexpress
-            Is safe to run multiple times because it makes sure not to have duplicates.
+            Add useful tool aliases
+
     #>
     param()
 
-    if (Get-Command docker -ErrorAction SilentlyContinue) {
-        New-Alias -Name 'd' -Value docker -Scope Global
-        Import-Module DockerCompletion
-        if (-not $?) {
-            Write-Warning "DockerCompletion module not found, installing."
-            Install-Module DockerCompletion -Scope CurrentUser
-            Import-Module DockerCompletion
-        }
+    $p = @{
+        SearchPath = @()
+        ToolName = 'docker.exe'
+        AliasName = 'd'
     }
+    New-MyProToolAlias @p
 
-    if (Get-Command docker-compose.exe -ErrorAction SilentlyContinue) {
-        New-Alias -Name dc -Value docker-compose.exe -Scope Global
+    $p = @{
+        SearchPath = @()
+        ToolName = 'docker-compose.exe'
+        AliasName = 'dc'
     }
+    New-MyProToolAlias @p
 
-    if (Test-Path 'c:\Program Files\Sublime Text 3\sublime_text.exe') {
-        New-Alias -Name 'st' -Value 'c:\Program Files\Sublime Text 3\sublime_text.exe' -Scope Global
+    $p = @{
+        SearchPath = @("$env:ProgramFiles\Sublime Merge")
+        ToolName = 'smerge.exe'
+        AliasName = 'sm'
     }
+    New-MyProToolAlias @p
 
-    if (Test-Path 'c:\Program Files\Sublime Merge\smerge.exe') {
-        New-Alias -Name 'sm' -Value 'c:\Program Files\Sublime Merge\smerge.exe' -Scope Global
+    $p = @{
+        SearchPath = @("$env:ProgramFiles\Notepad++")
+        ToolName = 'notepad++.exe'
+        AliasName = 'npp'
     }
+    New-MyProToolAlias @p
 
-    if (Get-Command -Name 'code-insiders.cmd' -ErrorAction SilentlyContinue) {
-        New-Alias -Name 'ci' -Value 'code-insiders.cmd' -Scope Global
+    $p = @{
+        SearchPath = @()
+        ToolName = 'Far.exe'
+        AliasName = 'far'
     }
+    New-MyProToolAlias @p
 
-    if (Test-Path 'C:\Program Files\Notepad++\notepad++.exe') {
-        New-Alias -Name 'npp' -Value 'C:\Program Files\Notepad++\notepad++.exe' -Scope Global
+    $p = @{
+        SearchPath = @()
+        ToolName = 'RegexBuddy4.exe'
+        AliasName = 'regbuddy'
     }
+    New-MyProToolAlias @p
 }
