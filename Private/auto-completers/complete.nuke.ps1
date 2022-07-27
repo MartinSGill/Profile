707d88dd3efc5e83fd163f1bdf5eea0cd53e890a
Martin Gill <martingill@groendus.nl>
2022-07-27 15:09:42 +0200

    Fix nuke auto-complete

--- Private/auto-completers/complete.nuke.ps1
+++ Private/auto-completers/complete.nuke.ps1
@@ -3,7 +3,7 @@ if (Test-MyProCommand nuke) {
     Write-MyProDebug "Found 'nuke', registering completer."
     Register-ArgumentCompleter -Native -CommandName nuke -ScriptBlock {
         param($commandName, $wordToComplete, $cursorPosition)
-            nuke :complete "$wordToComplete" | ForEach-Object {
+            nuke ':complete' "$wordToComplete" | ForEach-Object {
                 [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
             }
     }
